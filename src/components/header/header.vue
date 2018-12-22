7c4f92cd40dc16318c9cfe8d08e2fd2a84bd141f
Brian Best <brian.brianb64@gmail.com>
2018-12-21 21:51:20 -0500

    made header component

---
+++ src/components/header/header.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2 class="header" :class="size">
+    <slot></slot>
+  </h2>
+</template>
+
+<script>
+
+export default {
+  name: 'Header',
+  props:{
+    size: String
+  }
+}
+</script>
+
+<style>
+  .title {
+    font-size: 30px;
+  }
+
+</style>
