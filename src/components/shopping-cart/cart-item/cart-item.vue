98418e9b61f1cb6b6dae8fb6f2724b98189f8f77
Brian Best <brian.brianb64@gmail.com>
2018-12-22 17:09:50 -0500

    creates cart item component

---
+++ src/components/shopping-cart/cart-item/cart-item.vue
@@ -0,0 +1,45 @@
+<template>
+  <li class="cart-item" v-on:click="purchasedToggle" v-bind:class="classObject">
+    {{cartItem.name}}
+  </li>
+</template>
+
+<script>
+
+export default {
+  name: 'CartItem',
+  props: {
+    item: {
+      type: Object,
+      default: function () {
+        return { id: '', name: '',  purchased: false}
+      }
+    }
+  },
+  data: function() {
+    return {
+      cartItem: this.item,
+      classObject: {
+        purchased: this.cartItem.purchased
+      }
+    }
+  },
+  methods: {
+    purchasedToggle: function(){
+      console.log('i hear you', this.cartItem)
+      this.cartItem.purchased = true;
+    }
+  }
+}
+</script>
+
+<style>
+.cart-item {
+  list-style: none;
+}
+
+.cart-item.purchased {
+  opacity: 0.5;
+  text-decoration: line-through;
+}
+</style>
