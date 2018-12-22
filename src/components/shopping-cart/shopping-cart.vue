4dd7d5a0d81c6c8b7cd6eb030b680e2665b2241c
Brian Best <brian.brianb64@gmail.com>
2018-12-22 17:09:35 -0500

    create shopping cart component

---
+++ src/components/shopping-cart/shopping-cart.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul class="shopping-cart">
+    <CartItem
+      v-for="item in items"
+      v-bind:key="item.id"
+      v-bind:item="item"/>
+  </ul>
+</template>
+
+<script>
+import CartItem from './cart-item/cart-item.vue'
+
+export default {
+  name: 'ShoppingCart',
+  components: {
+    CartItem
+  },
+  props:{
+    items: Array
+  },
+  data: function() {
+    return {
+
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+
+</style>
