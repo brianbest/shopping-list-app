791c348be0d1d3d0ba2bf850311c077c4106c287
Brian Best <brian.brianb64@gmail.com>
2018-12-22 17:10:15 -0500

    adding via the form now does so using emit events

---
+++ src/components/add-item-form/AddItemForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="">
+    <input type="text" v-model="newItem" v-on:keydown.enter="confirm">
+    <button :disabled="newItem == ''" v-on:click="confirm">Add item</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'AddItemForm',
+  data: function(){
+    return {
+      newItem: ''
+    }
+  },
+  methods: {
+    confirm: function () {
+      //this.onConfirm(this.newItem)
+      this.$emit('confirm', this.newItem);
+    }
+  }
+}
+</script>
+
+<style>
+  .title {
+    font-size: 30px;
+  }
+
+</style>
